ad066f4ba795882576ab686fac6d2f63899df3b3
Virej Dasani <virej@Virejs-MacBook-Air.local>
2020-11-28 12:20:58 +0530

    Added HaveliActivity

---
+++ app/src/main/java/com/virej/pushtionthego/HaveliActivity.java
@@ -0,0 +1,14 @@
+package com.virej.pushtionthego;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HaveliActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_haveli);
+    }
+}
